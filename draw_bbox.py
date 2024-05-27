831df2ba3b521cfc22d3f6f3baffecfa63790546
AgniechaP <agnpior@gmail.com>
2024-05-27 17:29:48 +0200

    Changes in scripts - resolving errors in Colab

--- draw_bbox.py
+++ draw_bbox.py
@@ -4,6 +4,10 @@ import numpy as np
 # Create a black image of size 1024x1024 with 3 channels (BGR)
 image = np.zeros((1024, 1024, 3), dtype=np.uint8)
 
+image_test = np.zeros((1024,1024,3), dtype=np.uint8)
+
+image_read = cv2.imread('/home/agnieszka/Downloads/001_0015_0.png')
+
 # Define the bounding box values
 bbox = [932, 201, 8, 12]
 
@@ -15,9 +19,14 @@ bottom_right = (bbox[0] + bbox[2], bbox[1] + bbox[3])
 # cv2.rectangle(image, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255), -1)
 cv2.rectangle(image, top_left, bottom_right, (255, 0, 0), -1)
 
+# box = ['484', '359', '6', '8', '0']
+box = ['393', '83', '10', '11', '0']
+ctr_image = cv2.rectangle(image_read, (int(box[0]), int(box[1])), (int(box[2])+int(box[0]), int(box[3])+int(box[1])), (208, 146,0), 2)
+
+
 # Save or display the image to verify the result
 # cv2.imwrite('output.png', image)
 # To display the image, you can use the following line (uncomment if you want to see it in a window):
-cv2.imshow('Image with Blue Rectangle', image)
+cv2.imshow('Image with Blue Rectangle', ctr_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
